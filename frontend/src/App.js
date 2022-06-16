d047dc4dd55ad6c1ad1c94d123160ca14b250b62
Ming <ming000.zhang@gmail.com>
2022-06-16 16:28:34 +0800

    6-add-route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,28 @@
 
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        list products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+
+
+        </main>
+      </div>
+
+    </BrowserRouter>
+
   );
 }
 
