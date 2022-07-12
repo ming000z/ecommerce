c0d8b6498221529a7e10f32a86bad7d95b13778e
Ming <ming000.zhang@gmail.com>
2022-07-12 17:01:16 +0800

    26-create-payment-screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -13,6 +13,10 @@ const initialState = {
         ? JSON.parse(localStorage.getItem('shippingAddress'))
         : {},
 
+        paymentMethod: localStorage.getItem('paymentMethod')
+        ? localStorage.getItem('paymentMethod')
+        : '',
+
         cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
         : [],
@@ -49,6 +53,7 @@ function reducer(state, action){
                 cart: {
                     cartItems: [],
                     shippingAddress: {},
+                    paymentMethod: '',
                     },
             };
         case 'SAVE_SHIPPING_ADDRESS':
@@ -57,6 +62,14 @@ function reducer(state, action){
                 cart: {
                     ...state.cart,
                     shippingAddress: action.payload,
+                },
+            };
+        case 'SAVE_PAYMENT_METHOD':
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    paymentMethod: action.payload,
                 },
             };
         default:
